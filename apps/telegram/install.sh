14e7a6bbba3bef4df6b5a0099525aa939287a567
Boudjada Yasser <by.root96@gmail.com>
2022-08-02 20:18:36 +0100

    Add files via upload

--- apps/telegram/install.sh
+++ apps/telegram/install.sh
@@ -36,6 +36,7 @@ if [ "$1" == "-i" ] ; then
     sudo cp "$appname.png" "/usr/share/$appname/$appname.png";
     sudo cp "$appname.desktop" "/usr/share/applications/$appname.desktop";
     sudo cp "$appname.sh" "/usr/local/bin/$appname";
+    sudo chmod +x "/usr/local/bin/$appname";
 
 # uninstall:
 elif [ "$1" == "-u" ] ; then
